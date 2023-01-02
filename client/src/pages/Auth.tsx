10e86b29eb686bb2361acabcbb8a9c08e1e706e4
Gyuri092 <ggr0927@gmail.com>
2023-01-03 01:52:25 +0900

    feat: Assignment 1 완료

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,7 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Signup from './Signup';
-import Login from './Login';
 import LoginSignup from './LoginSignup';
 import styled from 'styled-components';
 
@@ -15,37 +13,11 @@ const Container = styled.div`
   border: 2px solid purple;
   border-radius: 10px;
 `;
-
-const LabelContainer = styled.div`
-  width: 40%;
-  height: 20%;
-  text-align: center;
-  font-size: 30px;
-`;
-
 function Auth() {
   const navigate = useNavigate();
-  const [formName, setFormName] = useState('');
   return (
     <Container>
-      <LabelContainer>
-        <button
-          onClick={() => {
-            setFormName('signup');
-          }}
-        >
-          회원가입
-        </button>
-        /
-        <button
-          onClick={() => {
-            setFormName('login');
-          }}
-        >
-          로그인
-        </button>
-      </LabelContainer>
-      <LoginSignup formName={formName} />
+      <LoginSignup />
       <button onClick={() => navigate('/')}>홈으로</button>
     </Container>
   );
--- client/src/pages/LoginSignup.tsx
+++ client/src/pages/LoginSignup.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
 import API from '../utils/customAxios';
 import axios, { AxiosError } from 'axios';
+import { useNavigate } from 'react-router-dom';
 const Label = styled.label`
   font-weight: bold;
   margin: 10px 10px;
@@ -29,9 +30,19 @@ const MainLabel = styled.div`
   display: block;
   font-size: 30px;
 `;
+
+const LabelContainer = styled.div`
+  width: 40%;
+  height: 20%;
+  text-align: center;
+  font-size: 30px;
+`;
 type ServerError = { errorMessage: string };
-function LoginSignup({ formName }: any) {
+function LoginSignup() {
+  const navigate = useNavigate();
+  const [formName, setFormName] = useState('');
   const formRef = useRef<HTMLFormElement | null>(null);
+  const [isDisable, setIsDisable] = useState(true);
   const [inputDatadValue, setInputDataValue] = useState({
     email: '',
     password: '',
@@ -42,15 +53,22 @@ function LoginSignup({ formName }: any) {
   };
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (localStorage.getItem('token')) {
+      navigate('/');
+    }
     try {
       await API.post(
         `/users/${formName === 'login' ? 'login' : 'create'}`,
         formData,
       ).then((res: any) => {
         if (res.status === 200) {
-          console.log('정상적으로 요청되었습니다.');
-          console.log('res.data : ', res.data.token);
+          localStorage.setItem('token', res.data.token);
           formRef?.current?.reset();
+          if (localStorage.getItem('token') !== res.data.token) {
+            alert('토큰이 유효하지 않습니다.');
+            navigate('/login');
+            localStorage.removeItem('token');
+          }
         }
       });
     } catch (error) {
@@ -62,7 +80,6 @@ function LoginSignup({ formName }: any) {
           alert(jsonObj.details);
         }
       }
-      return { errorMessage: 'Kuch to ghotala h' };
     }
   };
 
@@ -76,34 +93,68 @@ function LoginSignup({ formName }: any) {
         return { ...prev, password: e.target.value };
       });
     }
+    if (
+      inputDatadValue.email &&
+      inputDatadValue.password &&
+      inputDatadValue.email.includes('@') &&
+      inputDatadValue.email.includes('.') &&
+      inputDatadValue.password.length >= 8
+    ) {
+      setIsDisable(false);
+    } else {
+      setIsDisable(true);
+    }
   };
   return (
-    <SignupContainer>
-      <MainLabel> {formName === 'login' ? 'Login' : 'Sign up'}</MainLabel>
-      <form onSubmit={handleSubmit} ref={formRef}>
-        <Label>
-          이메일
-          <Input
-            type="email"
-            value={inputDatadValue.email}
-            onChange={onChange}
-            required
-          />
-        </Label>
-        <Label>
-          비밀번호
-          <Input
-            type="password"
-            value={inputDatadValue.password}
-            onChange={onChange}
-            required
-          />
-        </Label>
-        <button type="submit">
-          {formName === 'login' ? '로그인 하기' : '회원 가입'}
+    <>
+      <LabelContainer>
+        <button
+          onClick={() => {
+            setFormName('signup');
+            formRef?.current?.reset();
+            setInputDataValue({ email: '', password: '' });
+          }}
+        >
+          회원가입
         </button>
-      </form>
-    </SignupContainer>
+        /
+        <button
+          onClick={() => {
+            setFormName('login');
+            formRef?.current?.reset();
+            setInputDataValue({ email: '', password: '' });
+          }}
+        >
+          로그인
+        </button>
+      </LabelContainer>
+      <SignupContainer>
+        <MainLabel> {formName === 'login' ? 'Login' : 'Sign up'}</MainLabel>
+        <form onSubmit={handleSubmit} ref={formRef}>
+          <Label>
+            이메일
+            <Input
+              type="email"
+              value={inputDatadValue.email}
+              onChange={onChange}
+              required
+            />
+          </Label>
+          <Label>
+            비밀번호
+            <Input
+              type="password"
+              value={inputDatadValue.password}
+              onChange={onChange}
+              required
+            />
+          </Label>
+          <button type="submit" disabled={isDisable}>
+            {formName === 'login' ? '로그인 하기' : '회원 가입'}
+          </button>
+        </form>
+      </SignupContainer>
+    </>
   );
 }
 
