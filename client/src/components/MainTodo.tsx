35eec70c123d93fd434e872d5c7a42e20c8b62ab
Gyuri092 <ggr0927@gmail.com>
2023-01-03 23:59:22 +0900

    feat: create post

--- client/src/components/MainTodo.tsx
+++ client/src/components/MainTodo.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { HiPlus } from 'react-icons/hi';
 import { BiPencil } from 'react-icons/bi';
 import { BiTrash } from 'react-icons/bi';
+import API from '../utils/customAxios';
+import axios, { AxiosError } from 'axios';
+import { useNavigate } from 'react-router-dom';
 const MainTodoContainer = styled.div`
   width: 70%;
   height: 100%;
@@ -37,11 +40,78 @@ const ButtonContainer = styled.div`
   box-sizing: border-box;
 `;
 
+const ContentContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 4%;
+  padding: 5px 10px 5px 10px;
+  box-sizing: border-box;
+  font-size: 20px;
+`;
+
+const Textarea = styled.textarea`
+  width: 100%;
+  height: 89.5%;
+  border: none;
+  font-size: 16px;
+  padding: 10px;
+`;
+
+type ServerError = { errorMessage: string };
 function MainTodo() {
+  const token = localStorage.getItem('token');
+  const [inputDatadValue, setInputDataValue] = useState({
+    title: '',
+    content: '',
+  });
+  const formData = {
+    title: inputDatadValue.title,
+    content: inputDatadValue.content,
+  };
+  const AddTodo = async () => {
+    try {
+      await API.post('/todos', formData, {
+        headers: { Authorization: token },
+      }).then((res: any) => {
+        if (res.status === 200) {
+          console.log('정상적으로 처리되었습니다.');
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      if (axios.isAxiosError(error)) {
+        const serverError = error as AxiosError<ServerError>;
+        if (serverError && serverError.response) {
+          const jsonString = JSON.stringify(serverError.response.data);
+          const jsonObj = JSON.parse(jsonString);
+          alert(jsonObj.details);
+        }
+      }
+    }
+  };
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    if (e.target.name === 'title') {
+      setInputDataValue((prev) => {
+        return { ...prev, title: e.target.value };
+      });
+    } else {
+      setInputDataValue((prev) => {
+        return { ...prev, content: e.target.value };
+      });
+    }
+  };
   return (
     <MainTodoContainer>
       <ButtonContainer>
-        <Button>
+        <Button onClick={AddTodo}>
           Todo 추가 <HiPlus />
         </Button>
         <Button>
@@ -52,7 +122,23 @@ function MainTodo() {
           Todo 삭제 <BiTrash />
         </Button>
       </ButtonContainer>
-      <div>글 상세</div>
+      <ContentContainer>
+        <Input
+          type="text"
+          name="title"
+          value={inputDatadValue.title}
+          onChange={onChange}
+          placeholder="제목을 입력하세요..."
+        />
+        <Textarea
+          name="content"
+          id=""
+          cols={30}
+          value={inputDatadValue.content}
+          onChange={onChange}
+          placeholder="내용을 입력하세요..."
+        ></Textarea>
+      </ContentContainer>
     </MainTodoContainer>
   );
 }
